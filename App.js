c6775c2d3d37b4f9e041801da16afd11c37e5a13
EnterfaceLLC <dotenterface@gmail.com>
2023-11-20 15:44:18 -0600

    First set-up and testing createOpenLink; Permssion Config for Expo Location

--- App.js
+++ App.js
@@ -1,19 +1,62 @@
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
+
 import openMap, { createOpenLink, createMapLink } from "react-native-open-maps";
 
+import * as Location from "expo-location";
+
 export default function App() {
+  const [location, setLocation] = useState(null);
+  // const [errorMsg, setErrorMsg] = useState(null);
 
+  useEffect(() => {
+    const getPermission = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission denied");
+        return;
+      }
 
-  const evergy = { latitude: 39.050756, longitude: -95.672579 };
+      let currentLocal = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocal);
+      console.log("Permission granted", currentLocal);
+    };
 
-  const openRoute = createOpenLink({ zoom: 15, start: Platform.OS === "ios" ? '4827 SW Misty Harbor Ave Topeka, KS' : null, end: "Helping Hands Humane Society" });
+    getPermission();
+  }, []);
+
+  // const geoCode = async () => {
+  //   const geoLocal = await Location.geocodeAsync(myAddress);
+  //   console.log("Geo-Location:", geoLocal);
+  // };
+
+  // const reverseCode = async () => {
+  //   const reverseLocal = await Location.reverseGeocodeAsync({
+  //     latitude: location.coords.latitude,
+  //     longitude: location.coords.longitude,
+  //   }, {
+  //     useGoogleMaps: true,
+  //   });
+
+  //   console.log("ReverseGeo:", reverseLocal);
+  // };
+
+  const openRoute = createOpenLink({
+    zoom: 15,
+    provider: "google",
+    end: "Helping Hands Humane Society, Topeka, KS",
+  });
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Button color={"#bdc3c7"} onPress={openRoute} title="Click To Open Maps 🗺" />
+      <Button
+        color={"#bdc3c7"}
+        onPress={openRoute}
+        title="Click To Open Maps 🗺"
+      />
     </View>
   );
 }
